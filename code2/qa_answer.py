60674f1f66688444bc4c062cccbebf31193cd270
Ubuntu <salarcon@cs224n.5wdwgtp3ieoedayp3aw2nmriaf.jx.internal.cloudapp.net>
2017-03-22 12:07:46 +0000

    Submitted model. Finally done. Final scores on Test set: F1 = 66.239, EM = 54.6

--- code2/qa_answer.py
+++ code2/qa_answer.py
@@ -26,17 +26,30 @@ logging.basicConfig(level=logging.INFO)
 FLAGS = tf.app.flags.FLAGS
 
 tf.app.flags.DEFINE_float("learning_rate", 0.001, "Learning rate.")
-tf.app.flags.DEFINE_float("dropout", 0.15, "Fraction of units randomly dropped on non-recurrent connections.")
-tf.app.flags.DEFINE_integer("batch_size", 10, "Batch size to use during training.")
-tf.app.flags.DEFINE_integer("epochs", 0, "Number of epochs to train.")
-tf.app.flags.DEFINE_integer("state_size", 200, "Size of each model layer.")
-tf.app.flags.DEFINE_integer("embedding_size", 100, "Size of the pretrained vocabulary.")
+tf.app.flags.DEFINE_float("max_gradient_norm", 10.0, "Clip gradients to this norm.")
+tf.app.flags.DEFINE_float("dropout_keep_prob", 0.85, "Fraction of units randomly dropped on non-recurrent connections.")
+tf.app.flags.DEFINE_integer("batch_size", 20, "Batch size to use during training.")
+tf.app.flags.DEFINE_integer("epochs", 8, "Number of epochs to train.")
+tf.app.flags.DEFINE_integer("state_size", 150, "Size of each model layer.")
 tf.app.flags.DEFINE_integer("output_size", 750, "The output size of your model.")
-tf.app.flags.DEFINE_integer("keep", 0, "How many checkpoints to keep, 0 indicates keep all.")
-tf.app.flags.DEFINE_string("train_dir", "train", "Training directory (default: ./train).")
+tf.app.flags.DEFINE_integer("embedding_size", 100, "Size of the pretrained vocabulary.")
+tf.app.flags.DEFINE_integer("num_perspectives", 30, "Dimension of perspectives matrix in BMPM.")
+tf.app.flags.DEFINE_integer("num_perspective_classes", 6, "Dimension of perspectives matrix in BMPM.")
+tf.app.flags.DEFINE_string("data_dir", "data/squad", "SQuAD directory (default ./data/squad)")
+tf.app.flags.DEFINE_string("train_dir", "weights_65", "Training directory to save the model parameters (default: ./train).")
+tf.app.flags.DEFINE_string("load_train_dir", "train", "Training directory to load model parameters from to resume training (default: {train_dir}).")
 tf.app.flags.DEFINE_string("log_dir", "log", "Path to store log and flag files (default: ./log)")
+tf.app.flags.DEFINE_string("weights_dir", "weights", "Path to store weights (default: ./weights)")
+tf.app.flags.DEFINE_string("optimizer", "adam", "adam / sgd")
+tf.app.flags.DEFINE_integer("print_every", 1, "How many iterations to do per print.")
+tf.app.flags.DEFINE_integer("keep", 0, "How many checkpoints to keep, 0 indicates keep all.")
 tf.app.flags.DEFINE_string("vocab_path", "data/squad/vocab.dat", "Path to vocab file (default: ./data/squad/vocab.dat)")
-tf.app.flags.DEFINE_string("embed_path", "", "Path to the trimmed GLoVe embedding (default: ./data/squad/glove.trimmed.{embedding_size}.npz)")
+tf.app.flags.DEFINE_string("embed_path", "data/squad/glove.trimmed.100.npz", "Path to the trimmed GLoVe embedding (default: ./data/squad/glove.trimmed.{embedding_size}.npz)")
+tf.app.flags.DEFINE_string("max_paragraph_size", 300, "Path to the trimmed GLoVe embedding (default: ./data/squad/glove.trimmed.{embedding_size}.npz)")
+tf.app.flags.DEFINE_string("max_question_size", 25, "Path to the trimmed GLoVe embedding (default: ./data/squad/glove.trimmed.{embedding_size}.npz)")
+tf.app.flags.DEFINE_string("model", "bmpm", "Which baseline should we use")
+tf.app.flags.DEFINE_string("testing", "train", "Run on same batch.")
+tf.app.flags.DEFINE_integer("validation_sample", 200, "Sample size for validation batch.")
 tf.app.flags.DEFINE_string("dev_path", "data/squad/dev-v1.1.json", "Path to the JSON dev set to evaluate against (default: ./data/squad/dev-v1.1.json)")
 
 def initialize_model(session, model, train_dir):
@@ -131,6 +144,30 @@ def generate_answers(sess, model, dataset, rev_vocab):
     """
     answers = {}
 
+    raw_contexts, raw_questions, question_uuids = dataset
+    contexts = [[int(x) for x in raw_context.split()] for raw_context in raw_contexts]
+    questions = [[int(x) for x in raw_question.split()] for raw_question in raw_questions]
+    idx = 0    
+
+    while True:
+        if idx >= len(contexts): break
+        paragraph_batch = contexts[idx:idx+FLAGS.batch_size]
+        question_batch = questions[idx:idx+FLAGS.batch_size]
+        uuid_batch = question_uuids[idx:idx+FLAGS.batch_size]
+        
+        #paragraphs, paragraph_masks = model.mask_and_pad(paragraph_batch, 'paragraph')
+        #questions, question_masks = model.mask_and_pad(question_batch, 'question')
+        #a_s, a_e = model.answer(sess, paragraphs, paragraph_masks, questions, question_masks)
+        a_s, a_e = model.answer(sess, paragraph_batch, question_batch)
+        
+        for i in range(len(uuid_batch)):
+            uuid = uuid_batch[i]
+            context = paragraph_batch[i]
+            answer_int = context[a_s[i]:a_e[i]+1]
+            answers[uuid] = ' '.join([rev_vocab[ind] for ind in answer_int])
+        
+        idx += FLAGS.batch_size
+
     return answers
 
 
@@ -176,10 +213,12 @@ def main(_):
     # ========= Model-specific =========
     # You must change the following code2 to adjust to your model
 
-    encoder = Encoder(size=FLAGS.state_size, vocab_dim=FLAGS.embedding_size)
-    decoder = Decoder(output_size=FLAGS.output_size)
+    encoder = Encoder(size=FLAGS.state_size, vocab_dim=FLAGS.embedding_size, num_perspectives=FLAGS.num_perspectives,
+                      dropout_keep_prob=FLAGS.dropout_keep_prob)
+    decoder = Decoder(output_size=FLAGS.output_size, num_perspectives=FLAGS.num_perspectives,
+                      num_perspective_classes=FLAGS.num_perspective_classes,  hidden_state_size=FLAGS.state_size)
 
-    qa = QASystem(encoder, decoder)
+    qa = QASystem(encoder, decoder, FLAGS)
 
     with tf.Session() as sess:
         train_dir = get_normalized_train_dir(FLAGS.train_dir)
@@ -193,3 +232,4 @@ def main(_):
 
 if __name__ == "__main__":
   tf.app.run()
+
